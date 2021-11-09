dd1cbb6685363f57f8d257860c0a5161a0e6e0c8
Lewis Rafuse <lewisrafuse@gmail.com>
2021-11-09 17:37:52 -0500

    Continuous deploys test in correct dir.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ssh -o StrictHostKeyChecking=no ubuntu@34.205.100.22 << 'ENDSSH'
+  cd /home/ubuntu/safety-vision-infra
   docker login --username safetyvision --password $DOCKER_PASSWORD
   docker pull safetyvision/safety-vision-platform
   docker-compose -f docker-compose.prod.yml up -d web
