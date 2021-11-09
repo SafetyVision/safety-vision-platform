f9ffbd0c01084a9976cd8f87361f5cb46c9ea14c
Lewis Rafuse <lewisrafuse@gmail.com>
2021-11-09 18:12:27 -0500

    Remove docker ps.

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,4 @@ ssh -o StrictHostKeyChecking=no ubuntu@34.205.100.22 << 'ENDSSH'
   docker login --username safetyvision --password $DOCKER_PASSWORD
   docker pull safetyvision/safety-vision-platform
   docker-compose -f docker-compose.prod.yml up --no-deps -d web
-  docker ps
 ENDSSH
