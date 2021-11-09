29d8338db0710965c3031fd8340ac562a7b0f99e
Lewis Rafuse <lewisrafuse@gmail.com>
2021-11-09 17:44:40 -0500

    Add docker ps to ensure container running.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ ssh -o StrictHostKeyChecking=no ubuntu@34.205.100.22 << 'ENDSSH'
   docker login --username safetyvision --password $DOCKER_PASSWORD
   docker pull safetyvision/safety-vision-platform
   docker-compose -f docker-compose.prod.yml up -d web
+  docker ps
 ENDSSH
