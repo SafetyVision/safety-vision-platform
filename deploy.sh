4f931b0f8e19607a24228cd8f64f66cd424f3a98
Lewis Rafuse <lewisrafuse@gmail.com>
2021-11-09 17:47:49 -0500

    Add no deps flag for CD.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ ssh -o StrictHostKeyChecking=no ubuntu@34.205.100.22 << 'ENDSSH'
   cd /home/ubuntu/safety-vision-infra
   docker login --username safetyvision --password $DOCKER_PASSWORD
   docker pull safetyvision/safety-vision-platform
-  docker-compose -f docker-compose.prod.yml up -d web
+  docker-compose -f docker-compose.prod.yml up --no-deps -d web
   docker ps
 ENDSSH
