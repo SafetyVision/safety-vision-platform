c972ec827cdafbe7c7966ce71cb24d98da7ebc2f
Lewis Rafuse <lewisrafuse@gmail.com>
2021-11-10 11:47:48 -0500

    Push static file to S3 on each deploy.

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ ssh -o StrictHostKeyChecking=no ubuntu@34.205.100.22 << 'ENDSSH'
   docker login --username safetyvision --password $DOCKER_PASSWORD
   docker pull safetyvision/safety-vision-platform
   docker-compose up --no-deps -d web
+  docker-compose exec web python manage.py collectstatic --no-input
 ENDSSH
