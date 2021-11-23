7d4cefef8cc57e852d267f420aa6d12944a4244e
Lewis Rafuse <lewisrafuse@gmail.com>
2021-11-23 18:05:37 -0500

    Remove test app models migrations.

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('test_app.urls')),
     path('api/', include('authentication.urls')),
     path('api/users/', include('users.urls')),
     path('api/accounts/', include('accounts.urls')),
