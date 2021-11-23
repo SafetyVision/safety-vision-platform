067d17f248697d051c240a9fb3bb65f50e9a5ede
Lewis Rafuse <lewisrafuse@gmail.com>
2021-11-23 15:56:27 -0500

    Allow user updates.

--- users/serializers.py
+++ users/serializers.py
@@ -18,3 +18,11 @@ class UserSerializer(ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        ExtendedUser.objects.filter(id=instance.id).update(**validated_data)
+        if password:
+            instance.set_password(password)
+            instance.save()
+        return instance
