e14003014f1389111a69456e96be9bc3459eb683
Lewis <47062950+rafusel@users.noreply.github.com>
2021-11-23 18:07:47 -0500

    Remove test app models migrations. (#6)

--- test_app/models.py
+++ test_app/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-class TestModel(models.Model):
-    description = models.TextField()
-
-    def __str__(self):
-        return self.description
--- test_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.ListTestModel.as_view()),
-    path('<int:pk>/', views.DetailTestModel.as_view()),
-]
