35bf2f589fa1901092d4c22dac4ccfa5ec68d20d
Lewis Rafuse <lewisrafuse@gmail.com>
2021-11-23 15:18:43 -0500

    Only allow 1 users created on account register.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,20 +10,20 @@ class CreateAccountSerializer(serializers.ModelSerializer):
         model = Account
         fields = ['account_name', 'login_identifier', 'users']
 
+    def validate_users(self, users):
+        if len(users) != 1:
+            raise serializers.ValidationError("Must include exactly one user when registering a new account")
+        return users
+
     def create(self, validated_data):
-        user = validated_data.pop('users', None)
-        if user and user[0] and len(user) == 1:
-            account = Account.objects.create(**validated_data)
+        user = validated_data.pop('users', None)[0]
+        account = Account.objects.create(**validated_data)
+        password = user.pop('password')
+        user = ExtendedUser.objects.create(**user, account=account)
+        user.set_password(password)
+        user.save()
 
-            user = user[0]
-            password = user.pop('password')
-            user = ExtendedUser.objects.create(**user, account=account)
-            user.set_password(password)
-            user.save()
-
-            return account
-        else:
-            serializers.ValidationError("Must provide exactly one valid user when creating an account")
+        return account
 
 class GetUpdateDeleteAccountSerializer(serializers.ModelSerializer):
     class Meta:
