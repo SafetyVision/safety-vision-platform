41cd2648aa32d0c295a2f9be8757a39da7ffacfc
Lewis Rafuse <lewisrafuse@gmail.com>
2022-01-08 17:41:24 -0500

    Adds logout endpoint.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import set_csrf_token, login_view, CheckAuth
+from .views import set_csrf_token, login_view, logout_view, CheckAuth
 
 urlpatterns = [
   path('set-csrf/', set_csrf_token, name='Set-CSRF'),
   path('login/', login_view, name='Login'),
+  path('logout/', logout_view, name='Logout'),
   path('test-auth/', CheckAuth.as_view(), name='Test-Auth')
 ]
