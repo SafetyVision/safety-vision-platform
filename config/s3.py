25600af7e7c9eaf9b7bc376faa19d6c33b4e7373
Lewis <47062950+rafusel@users.noreply.github.com>
2022-01-13 23:41:41 -0500

    Use boto3 for production storage. (#11)

--- config/s3.py
+++ config/s3.py
@@ -1,4 +1,4 @@
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3BotoStorage
 
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
