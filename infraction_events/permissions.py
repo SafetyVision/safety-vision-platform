1213118973d69f15ab48588b42caa5697be6b0ee
Lewis Rafuse <lewisrafuse@gmail.com>
2022-01-17 02:49:57 -0500

    Remove errant print.

--- infraction_events/permissions.py
+++ infraction_events/permissions.py
@@ -3,7 +3,6 @@ from config.settings import CREATE_INFRACTION_EVENT_KEY
 
 class IsPredictionServiceRequest(permissions.BasePermission):
   def has_permission(self, request, _):
-    print('poop')
     if 'HTTP_X_CREATE_INFRACTION_EVENT_KEY' in request.META:
       return request.META['HTTP_X_CREATE_INFRACTION_EVENT_KEY'] == CREATE_INFRACTION_EVENT_KEY
     return False
