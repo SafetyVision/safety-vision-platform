e077960d309cd1d8e2d34f02f3439f683b63f9cf
Andrew Hum <huma3@mcmaster.ca>
2022-01-19 20:23:33 -0500

    Removed device_id

--- infraction_types/models.py
+++ infraction_types/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class InfractionType(models.Model):
     infraction_type_name = models.CharField(max_length=100)
-    device_id = models.CharField(max_length=50)
+    # device_id = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     # updated_at = models.DateTimeField(auto_now=True)
     # Parameters to add later
--- infraction_types/serializers.py
+++ infraction_types/serializers.py
@@ -4,10 +4,10 @@ from .models import InfractionType
 class ListInfractionTypesSerializer(serializers.ModelSerializer):
     class Meta:
         model = InfractionType
-        fields = ['id', 'infraction_type_name', 'device_id', 'created_at']
-
+        # fields = ['id', 'infraction_type_name', 'device_id', 'created_at']
+        fields = ['id', 'infraction_type_name', 'created_at']
 class CreateInfractionTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = InfractionType
-        fields = ['infraction_type_name', 'device_id']
+        fields = ['infraction_type_name']
 
