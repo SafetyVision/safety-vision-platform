e81a0d1371b73c3d375c299350fe508f2e141305
StephanJon <stephanus.jonatan@gmail.com>
2022-01-23 23:06:49 -0500

    Update devices columns
    Included serial number and stream arn | removed stream name | override create and delete function to create/delete kinesis stream

--- devices/models.py
+++ devices/models.py
@@ -1,10 +1,12 @@
+from enum import unique
 from django.db import models
 import boto3
 from botocore.config import Config
 
 class Device(models.Model):
+    serial_number = models.CharField(max_length=1000, unique=True, default='')
     location = models.CharField(max_length=1000)
-    stream_name = models.CharField(max_length=1000)
+    stream_arn = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -16,7 +18,7 @@ class Device(models.Model):
             )
 
             endpoint_response = client.get_data_endpoint(
-                StreamName=self.stream_name,
+                StreamARN=self.stream_arn,
                 APIName='GET_HLS_STREAMING_SESSION_URL'
             )
             endpoint_url = endpoint_response['DataEndpoint']
@@ -28,7 +30,7 @@ class Device(models.Model):
             )
         
             stream_response = client.get_hls_streaming_session_url(
-                StreamName=self.stream_name,
+                StreamARN=self.stream_arn,
                 PlaybackMode='LIVE',
                 ContainerFormat='FRAGMENTED_MP4',
                 DiscontinuityMode='ALWAYS',
--- devices/serializers.py
+++ devices/serializers.py
@@ -1,15 +1,86 @@
+from asyncio import sleep
+from socket import timeout
 from rest_framework import serializers
 from .models import Device
+import boto3
+from botocore.config import Config
 
 class CreateDeviceSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Device
-        fields = ['location', 'stream_name']
+        fields = ['serial_number', 'location']
+
+    def create(self, validated_data):
+        region = 'us-east-1'
+        stream_name='SafetyVision-VS-'+validated_data['serial_number']
+        def getStreamEndpoint(client):
+            return  client.get_data_endpoint(
+                        StreamName=stream_name,
+                        APIName='GET_HLS_STREAMING_SESSION_URL'
+                    )
+        client = boto3.client(
+            'kinesisvideo',
+            config=Config(region_name=region),
+        )
+        try:
+            stream = client.describe_stream(
+                        StreamName=stream_name
+                    )
+            streamArn = stream['StreamInfo']['StreamARN']
+        except:
+            print("-------------------------------------\n\
+                no stream with name: " + stream_name +\
+                "\nCreating new stream...")
+            stream = client.create_stream(
+                StreamName=stream_name,
+                DataRetentionInHours=1
+            )
+            streamArn = stream['StreamARN']
+
+        endpoint_response = getStreamEndpoint(client)
+        endpoint_url = endpoint_response['DataEndpoint']
+        
+        counter = 0
+        timeout = 60
+        while counter < timeout:
+            try:
+                client2 = boto3.client(
+                    'kinesis-video-archived-media',
+                    endpoint_url=endpoint_url,
+                    config=Config(region_name=region)
+                )
+            
+                client2.get_hls_streaming_session_url(
+                    StreamName=stream_name,
+                    PlaybackMode='LIVE',
+                    ContainerFormat='FRAGMENTED_MP4',
+                    DiscontinuityMode='ALWAYS',
+                    DisplayFragmentTimestamp='NEVER',
+                    Expires=43100,
+                    MaxMediaPlaylistFragmentResults=3
+                )
+                break
+            except:
+                counter += 1
+                sleep(1)
+                
+        if counter == timeout:
+            client.delete_stream(
+                    StreamARN=streamArn
+                )
+            raise serializers.ValidationError("Device [" + validated_data['serial_number'] + "] not streaming")
+
+        device = Device(
+            **validated_data,
+            stream_arn=streamArn
+        )
+        device.save()
+
+        return device
 
 class DeviceSerializer(serializers.ModelSerializer):
     stream_url = serializers.CharField(source='getStreamUrl', max_length=1000)
 
     class Meta:
         model = Device
-        fields = ['id', 'location', 'stream_name','stream_url']
+        fields = ['id', 'serial_number', 'location','stream_url']
