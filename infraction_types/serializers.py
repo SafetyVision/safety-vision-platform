f8e9a8f8bb995e9e0c8ce7e03ad2267884efd4ac
Lewis <47062950+rafusel@users.noreply.github.com>
2022-01-24 18:53:34 -0500

    First demo last minute changes. (#20)
    * First demo last minute changes.
    
    * Add requests dep.

--- infraction_types/serializers.py
+++ infraction_types/serializers.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
 from .models import InfractionType
+import requests
 
 class ListInfractionTypesSerializer(serializers.ModelSerializer):
     class Meta:
         model = InfractionType
-        # fields = ['id', 'infraction_type_name', 'device_id', 'created_at']
-        fields = ['id', 'infraction_type_name', 'created_at']
+        fields = ['id', 'infraction_type_name', 'device', 'created_at']
+
 class CreateInfractionTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = InfractionType
-        fields = ['infraction_type_name']
+        fields = ['infraction_type_name', 'device']
 
+    def create(self, validated_data):
+        request_account_id = self.context['request'].user.account.id
+        infraction_type = InfractionType.objects.create(**validated_data)
+
+        url = "http://ec2-3-80-89-68.compute-1.amazonaws.com:5000/train_new"
+        json = {
+            'kvs_arn': infraction_type.device.stream_arn,
+            'infraction_type': infraction_type.id,
+            'account_id': request_account_id,
+            'location': infraction_type.device.id,
+        }
+
+        requests.post(url=url, json=json)
+
+        return infraction_type
