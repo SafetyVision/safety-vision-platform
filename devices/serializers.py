e49a957b327ab7c70b44e97e94b81b6441f0553e
Lewis Rafuse <lewisrafuse@gmail.com>
2022-02-19 16:55:20 -0500

    Readd stream_url field.

--- devices/serializers.py
+++ devices/serializers.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
 from .models import Device
-from locations.models import Location
 import boto3
 from botocore.config import Config
 
 
 class DeviceSerializer(serializers.ModelSerializer):
+    stream_url = serializers.CharField(source='getStreamUrl', max_length=1000)
+
     class Meta:
         model = Device
-        fields = ['id', 'serial_number', 'location', 'description']
-        read_only_fields = ['id', 'serial_number']
+        fields = ['id', 'serial_number', 'location', 'description', 'stream_url']
+        read_only_fields = ['id', 'serial_number', 'stream_url']
 
     def update(self, instance, validated_data):
         region = 'us-east-1'
