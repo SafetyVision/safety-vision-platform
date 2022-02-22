514e4441ef0c99ac49893896cbab89841c121893
Lewis Rafuse <lewisrafuse@gmail.com>
2022-02-21 19:00:24 -0500

    Adds account owner permissions.

---
+++ users/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+class IsAccountOwner(permissions.BasePermission):
+    def has_permission(self, request, _):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.account.owner.id == request.user.id
+
+class UserPermissions(permissions.BasePermission):
+    def has_object_permission(self, request, _, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if request.user.account.owner.id == request.user.id:
+            return True
+
+        if obj.id == request.user.id:
+            if request.method == 'DELETE':
+                return False
+            return True
+
+        return False
